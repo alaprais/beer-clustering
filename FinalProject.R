cb0715fbdc2f539e5c4fc3ea31701cd5e3ad37ad
Arnaud Laprais <32946879+alaprais@users.noreply.github.com>
2021-11-16 01:34:46 -0800

    Update Data Cleaning
    Added in Evans code. Added code to restrict attention to 10 arbitrary common beers.

--- FinalProject.R
+++ FinalProject.R
@@ -1,20 +1,75 @@
 ###################FinalProject################################
 
-data=read.csv("C:/Users/adam.aslam/OneDrive - Danaher/Desktop/MATH 267/recipeData.csv")
-data[,13]=as.numeric(data[,13])
-
-#HERE        put in code to remove N/As and bad data. call it data
-
-
-tbl=table(data[,5])
-top20=as.numeric(names(head(sort(tbl,decreasing=TRUE), n = 20)))
-data=(data[data$StyleID %in% top20,])
-#View(data)
-style=data$Style
-n=nrow(data)
-
-#install.packages("Mclust")
+### Personal Path ##################
+personal_path <- "C:/Users/Arnaud Laprais/Desktop/SJSU/267 computational stats/beer_project/recipeData.csv"
+##################################
+####### Libraries ################
+library(dplyr)
+library(tidyr)
 library(mclust)
+library(klaR)
+library(psych)
+library(MASS)
+library(devtools)
+library(ggplot2)
+##################################
+###################################################### START DATA CLEANING ############################################
+data <- read.csv(personal_path)
+
+
+# Keeping numeric/useful columns
+data <- data[,c(4, 7:13, 15)]
+
+
+#NAs are listed as "N/A" characters - convert to NA:
+data[data=="N/A"] = NA 
+
+
+# Percentage of NAs by column
+round(colSums(is.na(data))/nrow(data), 3)
+# There are just a few in Style so get rid of them
+data <- drop_na(data)
+
+
+# Water has a specific gravity of 1.000 and When grains for the wort are added, the density increases 
+# There should not be any beers with an original gravity of 1.000. 
+# High gravity beers have an OG of 1.075 so anything greater than 1.1 seems pretty unreasonable
+data <- data[data$OG>1,]
+data <- data[data$OG<1.1,]
+
+
+# FG can be less than one due to alcohol content - there's one listed with FG<0.5 that doesn't make sense so remove it:
+data <- data[data$FG>.5,]
+
+
+
+# Focus just on Alcoholic beers - remove all beers with ABV < 0.5
+data <- data[data$ABV>.5,]
+
+
+# IBU is kinda murky - almost all beers fall between 5 and 120, but can get up into the hundreds (and apparently a
+# couple in the thousands - but since these are homebrews not sure what's reasonable)
+data <- data[data$IBU<500,]
+
+
+# Color is generally between 0 - 40 but can get higher. Like IBU, not sure where we should draw the line (theres only 1 over 100 and it's
+# pretty suspect so removed it).
+data <- data[data$Color<100,]
+summary(data)
+
+# Keep 10 styles only
+to_keep <- c("American Pale Ale","Saison","American Amber Ale", "Blonde Ale", 
+             "Irish Red Ale", "Witbier", "Russian Imperial Stout", "Robust Porter",
+             "Kölsch", "Cream Ale")
+
+data <- data[data$Style %in% to_keep,]
+
+# count of each style in the data
+sort(table(data$Style), decreasing = TRUE) 
+
+#########################################  END DATA CLEANING ###############################################################
+
+######################################## START LDA/VARIABLE SELECTION ######################################################
 
 #result <- MclustDA(data[,c(6:12,15)], data[,4], modelType = "EDDA", modelNames = "EEE")
 
@@ -37,13 +92,6 @@ library(mclust)
 #install.packages("MASS")
 #install.packages("devtools")
 #install.packages("ggplot2")
-
-library(klaR)
-library(psych)
-library(MASS)
-library(devtools)
-library(ggplot2)
-
 
 
 LDAall = lda(data$Style~., data[,c(6:13,15)])
@@ -68,9 +116,5 @@ train=data[sample(1:n,ceiling(.8*n)),]
 indicesTest=c(1:n)[-trainSample]
 test=data[indicesTest,]
 
-#HERE   RUN EM ON TRAINING AND TEST SET WITH TEST SET LABELS REMOVED
-#EMData= data[,c(4,8,9,11)]
-
-
-
+#################################### END LDA/ VARIABLE SELECTION ########################################################
 
